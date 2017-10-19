48ca64a06823dda92159f0cd77b7d8b289b98120
Emanuela Ianc <eianc@zonedigital.com>
2017-10-19 21:47:14 +0100

    renamed the json data and amended the JS to match the new names. created the function which add the class active to the button based on the year is rendered

--- javascript/modules/world-arms.js
+++ javascript/modules/world-arms.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 
-let width = 600;
-let height = 600;
+let width = (window.innerWidth * 47) / 100;
+let height = window.innerHeight - 100;
 
 export default function arms() {
 
@@ -29,7 +29,7 @@ export default function arms() {
     let maxExportYears = json.map(function (obj) {
       let maxByYear = []
         obj.entries.map( function(entry) {
-          maxByYear.push(parseFloat(entry.export));
+          maxByYear.push(parseFloat(entry.exported));
         })
         var sum = d3.sum(maxByYear, function(d) { return d; })
       return d3.max(maxByYear, function(d) { return d; });
@@ -42,7 +42,7 @@ export default function arms() {
     let maxImportYears = json.map(function (obj) {
       let maxByYear = []
         obj.entries.map( function(entry) {
-          maxByYear.push(parseFloat(entry.import));
+          maxByYear.push(parseFloat(entry.imported));
         })
         var sum = d3.sum(maxByYear, function(d) { return d; })
       return d3.max(maxByYear, function(d) { return d; });
@@ -57,26 +57,31 @@ export default function arms() {
     /// create the scale for the radius
     let scaleRadius = d3.scaleSqrt().domain([0, maximum]).range([0,150]);
 
+
     /// set-up the forces
     let forceX = d3.forceX(width / 2).strength(0.6);
     let forceY = d3.forceY(height / 2).strength(0.4);
-    let forceExportCollide = d3.forceCollide(function(d) { return scaleRadius(d.export) + 2; });
-    let forceImportCollide = d3.forceCollide(function(d) { return scaleRadius(d.import) + 2; });
+    let forceExportCollide = d3.forceCollide(function(d) { return scaleRadius(d.exported) + 2; });
+    let forceImportCollide = d3.forceCollide(function(d) { return scaleRadius(d.imported) + 2; });
+    let currentYear;
 
-    /// getting the data only for a year
+    /// getting the data entries array only for a year
     function dataEntries(data, year = '2016') {
       let entriesByYear;
       data.map(function(d) {
         if (d.year === year) {
-          entriesByYear = d.entries;
+          currentYear = year;
+          entriesByYear = d.entries;        
         }
       })
       return entriesByYear;
     };
-
+    console.log(currentYear, 'currentYear');
     /// save the entries by year in this newData variable
     let newData = dataEntries(data, undefined);
+    console.log(currentYear, 'currentYear');
     
+    /// append the years list to the DOM
     let yearlist = yearsTrade.selectAll('li')
                   .data(data)
                   .enter().append('li')
@@ -90,19 +95,34 @@ export default function arms() {
     let buttonList = yearlist._groups[0];
     let buttonListLength = buttonList.length;
     let buttonListMiddle = Math.floor(buttonList.length / 2);
+    console.log(buttonList[10].className, 'buttonList');
 
     buttonList.map(function(list, index) {
-
+      
+      function addActiveClass(id, currentYear) {
+        for (let i=0; i< buttonList.length; i++) {
+          buttonList[i].className = 'button';
+        }
+        if(id === currentYear) {
+          list.className += ' active';
+        }
+      }
+      
+      addActiveClass(list.dataset.id, currentYear);
       /// display only the first 5 years, the middle 5 years and the last 5 years
       if ( ( ( index >= 5 ) && ( index <= (buttonListMiddle - 3) ) ) ||
            ( ( index >= (buttonListMiddle + 3) ) && ( index <= ( buttonListLength - 6 ) ) )) {
         list.parentNode.className = 'show-elipses';
         list.innerHTML = '.';
+        
       }
-
       list.onclick = function() {
         /// get data based by the year we clicked
         newData = dataEntries(data, list.dataset.id);
+
+        currentYear = list.dataset.id;
+        addActiveClass(list.dataset.id, currentYear);
+        console.log('currentYear', currentYear);
         /// clear the svg group 
         d3.selectAll('svg g > *').remove();
         /// calling the export and import functions
@@ -111,32 +131,28 @@ export default function arms() {
 
         if ( (index > 3) && (index < (buttonListLength - 4))) {
 
-        console.log(index, 'index');
-        
-        let currentYearIndex = index;
-        console.log(currentYearIndex, 'currentYearIndex');
-        let nextTwoSiblings = index + 1;
-        console.log(nextTwoSiblings, 'nextTwoSiblings');
-        let prevTwoSiblings = index - 1;
-        console.log(prevTwoSiblings, 'prevTwoSiblings');
-        let nextForthSibling = index + 3;
-        let prevForthSibling = index - 3;
+          
+          let currentYearIndex = index;
+          let nextSibling = index + 1;
+          let prevSibling = index - 1;
+          let nextThirdSibling = index + 3;
+          let prevThirdSibling = index - 3;
 
-        for (let i = prevTwoSiblings; i <= nextTwoSiblings; i++ ) {
-          showYears(i);
-        }
-        
-        if (prevForthSibling > 2) {        
-          for (let i = prevForthSibling; i < prevTwoSiblings; i++) {
-            hideYears(i);       
+          for (let i = prevSibling; i <= nextSibling; i++ ) {
+            showYears(i);
           }
-        }
+          
+          if (prevThirdSibling > 2) {        
+            for (let i = prevThirdSibling; i < prevSibling; i++) {
+              hideYears(i);       
+            }
+          }
 
-        if (nextForthSibling < (buttonListLength - 3)) {
-          for (let i = nextTwoSiblings + 1; i <= nextForthSibling; i++) {
-            hideYears(i);       
+          if (nextThirdSibling < (buttonListLength - 3)) {
+            for (let i = nextSibling + 1; i <= nextThirdSibling; i++) {
+              hideYears(i);       
+            }
           }
-        }
         }
       };
 
@@ -163,14 +179,14 @@ export default function arms() {
       let balloonsExport = armsExport.selectAll('.balloon')
     						          .data(data)
     						          .enter().append('circle')
-                          .attr('r', function(d) { return scaleRadius(d.export); })
+                          .attr('r', function(d) { return scaleRadius(d.exported); })
             		          .attr('fill', function(d) { return colorFill(d); })
     	
       /// append the title for each country that exports
-      balloonsExport.append('title').text(function(d) { return d.country; });
+      balloonsExport.append('title').text(function(d) { return d.country + ' ' + d.exported; });
       
       /// remove the circles that have the export 0
-      balloonsExport.filter(function(d) { return d.export === '0'; }).remove();
+      balloonsExport.filter(function(d) { return d.exported === '0'; }).remove();
 
       simulationExport.nodes(data)
         .on('tick', function() {
@@ -190,14 +206,14 @@ export default function arms() {
       let ballonsImport = armsImport.selectAll('.balloon')
                           .data(data)
                           .enter().append('circle')
-                          .attr('r', function (d) { return scaleRadius(d.import); })
+                          .attr('r', function (d) { return scaleRadius(d.imported); })
                           .attr('fill', function(d) { return colorFill(d); });
       
       /// append the title for each country that imports
       ballonsImport.append('title').text(function(d) { return d.country; });
 
       /// remove the circles that have the import 0
-      ballonsImport.filter(function (d) { return d.import === '0'; }).remove();
+      ballonsImport.filter(function (d) { return d.imported === '0'; }).remove();
 
       simulationImport.nodes(data)
         .on('tick', function () {
@@ -219,7 +235,7 @@ export default function arms() {
       } else if (d.continent === 'South America') {
         return '#75457C';
       } else if (d.continent === 'Oceania') {
-        return '#009CDF';
+        return '#114483';
       }
     }
 
